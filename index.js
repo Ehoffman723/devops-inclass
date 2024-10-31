7b9428e6e18136a2adeb01a35a8a5f8d697f05be
Ethan Hoffman <112048289+Ehoffman723@users.noreply.github.com>
2024-10-31 14:20:09 +0000

    initial files

---
+++ index.js
@@ -0,0 +1,13 @@
+exports.factorial = function(n) {
+    if(n < 0) {
+        return "Factorial for negative number is not defined"
+    }else if (n === 0 || n === 1) {
+        return 1;
+    } else {
+        let result = 1;
+        for (let i = 2; i <=n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+}
---
+++ test/test.js
@@ -0,0 +1,14 @@
+var main = require('../index');
+var assert = require('assert');
+describe('Regular factorial value', function() {
+    it('should return 1 for Fact(0)', function() {
+        assert.strictEqual(main.factorial(0), 1);
+      });
+});
+
+describe('First two terms', function() {
+  it('should return 1 and 1 for first two terms', function() {
+      assert.strictEqual(main.factorial(0), 1);
+      assert.strictEqual(main.factorial(1), 1);
+    });
+});
